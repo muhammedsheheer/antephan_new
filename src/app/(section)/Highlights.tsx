0396a8e416c7a608aebc440a5284246853e8012a
MernStack123 <stackmern213@gmail.com>
2025-05-13 20:36:59 +0530

    highlight updated

--- src/app/(section)/Highlights.tsx
+++ src/app/(section)/Highlights.tsx
@@ -152,11 +152,11 @@ const Highlights: React.FC = () => {
           viewport={{ once: false }}
         >
           <p className="w-full max-w-[400px] text-center font-inter text-sm font-[400] text-[#000] md:text-base">
-            Tired of dessert spots that serve the same factory-made sweetg with
-            no real flavor or soul? At Antephan, we break the mold with.
-            handcrafted Turkish desserts made from authentic recipes, real
-            ingredients, and a whole lot of love. Every bite is a journey — not
-            just sugar.
+            Tired of dessert spots serving the same factory-made sweets with no
+            real flavor or soul? At Antephan, we break the mold with handcrafted
+            Turkish desserts made from authentic recipes, real ingredients, and
+            a whole lot of love. Every bite is a journey — not just a hit of
+            sugar.
           </p>
           <div>
             <Button className="z-50 rounded-none bg-[#079EBC] px-6 py-6 text-center font-poppins text-sm font-[500] uppercase tracking-[1.276px] text-[#FFFAF0] hover:bg-[#1bd9ff] md:px-7 md:py-6">
